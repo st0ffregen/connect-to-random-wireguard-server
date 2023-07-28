4dd0f0ba61408ea8dd9249343d2f6ffa18e1426f
st0ffregen <stoffregen@mailbox.org>
2023-07-28 23:43:14 +0200

    remove german prefix from files when shuffling, generalize closing the connection

--- wireguard_disconnect_from_ovpn.sh
+++ wireguard_disconnect_from_ovpn.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-for iface in $(ifconfig | cut -d ' ' -f1| tr ':' '\n' | awk NF)
+# Create an array of names from the files in /etc/wireguard/ that end with .conf
+wireguard_files=(/etc/wireguard/*.conf)
+
+# Remove the file path from each element in the wireguard_files array
+wireguard_files=("${wireguard_files[@]##*/}")
+
+# Remove the file extension (.conf) from the file names
+wireguard_files=("${wireguard_files[@]%.conf}")
+
+# Use "ip addr" to get the list of interface names
+interfaces=($(ip addr | awk '/^[0-9]+:/ {gsub(/:/,"",$2); print $2}'))
+
+for iface in "${interfaces[@]}"
 do
-	if [[ $iface = vpn* ]]
-	then
-	        wg-quick down $iface
-	fi
+        # Check if $iface exists in the array of wireguard_files
+        if [[ " ${wireguard_files[*]} " == *" $iface "* ]]
+        then
+                wg-quick down $iface
+        fi
 done
